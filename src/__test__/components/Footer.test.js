382865c001f2758874377b1df6b645e1f76eaa63
CPerezUEX <cperez@urbano.com.sv>
2020-05-22 10:41:48 -0600

    add test for footer, header and products

---
+++ src/__test__/components/Footer.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { mount } from "enzyme";
+import Footer from "../../components/Footer";
+
+describe("<Footer/>", () => {
+  const footer = mount(<Footer />);
+  test("Render de componente footer", () => {
+    expect(footer.length).toEqual(1);
+  });
+  test("render del titulo", () => {
+    expect(footer.find(".Footer-title").text()).toEqual("Platzi Store");
+  });
+});
